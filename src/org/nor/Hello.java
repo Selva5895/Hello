db2925be66c4094dd35e88ab96ef009d4a1b17c2
Selva5895 <gselvabca03@gmail.com>
2021-07-26 07:34:41 +0530

    How r u

---
+++ src/org/nor/Hello.java
@@ -0,0 +1,12 @@
+package org.nor;
+
+public class Hello {
+	private void Company() {
+	System.out.println("Hello");
+}
+public static void main(String[] args) {
+	Hello h = new Hello();
+	h.Company();
+	
+}
+}
