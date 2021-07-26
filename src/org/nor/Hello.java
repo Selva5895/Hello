411f4dc14d297013e3078a1af997a20c1936a8d1
Selva5895 <gselvabca03@gmail.com>
2021-07-26 08:34:07 +0530

    key

--- src/org/nor/Hello.java
+++ src/org/nor/Hello.java
@@ -3,10 +3,15 @@ package org.nor;
 public class Hello {
 	private void Company() {
 	System.out.println("Hello");
+	
+}
+private void employee() {
+	System.out.println("python");
 }
 public static void main(String[] args) {
 	Hello h = new Hello();
 	h.Company();
+	h.employee();
 	
 }
 }
