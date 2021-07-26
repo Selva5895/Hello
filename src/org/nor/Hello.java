ac990d2c036a68ba2d6a9d4920197992d6d727c0
Selva5895 <gselvabca03@gmail.com>
2021-07-26 22:59:05 +0530

    changed

--- src/org/nor/Hello.java
+++ src/org/nor/Hello.java
@@ -4,9 +4,12 @@ public class Hello {
 	private void Company() {
 	System.out.println("Hello");
 }
+	private void organization() {
+	System.out.println("world");
+	}
 public static void main(String[] args) {
 	Hello h = new Hello();
 	h.Company();
-	
+	h.organization();
 }
 }
